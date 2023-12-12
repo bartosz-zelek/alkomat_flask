2d698289f682c388c19b4095dfaedf5c1b084345
JakubWieclaw <jwieclaw000@gmail.com>
2023-12-12 01:22:27 +0100

    add possibility to get records from specific user

--- api.py
+++ api.py
@@ -10,17 +10,23 @@ api = Blueprint('api', __name__)
 
 
 # Define a route to get readings from the database
-@api.route("/get_readings")
+@api.route("/get_readings", defaults={"id": None})
+@api.route("/get_readings/<int:id>")
 @login_required  # Add this decorator to protect the route
-def get_readings():
+def get_readings(id):
     try:
         count = request.args.get("count", default=50, type=int)
         offset = request.args.get("offset", default=0, type=int)
         # Try to get readings from the database
         db = get_db()
-        cur = db.execute(
-            "SELECT date_time, name, surname, value FROM users INNER JOIN readings ON users.rfid = readings.fk_rfid ORDER BY readings.date_time DESC LIMIT ? OFFSET ?", (count, offset)
-        )
+        if id:
+            cur = db.execute(
+                "SELECT date_time, name, surname, value FROM users INNER JOIN readings ON users.rfid = readings.fk_rfid WHERE users.rfid = ? ORDER BY readings.date_time DESC LIMIT ? OFFSET ?", (id, count, offset)
+            )
+        else:
+            cur = db.execute(
+                "SELECT date_time, name, surname, value FROM users INNER JOIN readings ON users.rfid = readings.fk_rfid ORDER BY readings.date_time DESC LIMIT ? OFFSET ?", (count, offset)
+            )
         list_of_readings = cur.fetchall()
         return jsonify(list_of_readings), 200
     except sqlite3.Error as er:
@@ -28,11 +34,8 @@ def get_readings():
         exc_type, exc_value, exc_tb = sys.exc_info()
         return traceback.format_exception(exc_type, exc_value, exc_tb)[-1], 500
     
-    # API pozawala na sprawdzenie:
 
-    # Czy RFID istnieje
-    # Czy RFID jest zablokowany/odblokowany
-@api.route("/check_rfid/<rfid>")
+@api.route("/check_rfid/<int:rfid>")
 def check_rfid(rfid):
     try:
         db = get_db()
