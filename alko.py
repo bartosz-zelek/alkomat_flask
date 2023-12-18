868cf0506509b442f42b958fbf4dec9440bdd1a3
JakubWieclaw <jwieclaw000@gmail.com>
2023-12-18 07:43:01 +0100

    delete testing script from repository

--- alko.py
+++
@@ -1,9 +0,0 @@
-import requests
-from time import sleep
-from random import uniform
-
-#print(requests.get('http://localhost:5000/add_employee/1/John/Doe'))
-
-for _ in range(8):
-    print(requests.get(f'http://localhost:5000/api/add_reading/AAAAAAAA/{uniform(0.0, 0.3):.2f}'))
-    #sleep(0.25)
